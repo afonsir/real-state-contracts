273056d9cb37373dbf6303249f7183897d41c93a
AfonSir <afonsir17@gmail.com>
2025-11-07 22:17:27 -0300

    refactor: improve buyer validations

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -4,8 +4,8 @@ class Buyer < ApplicationRecord
   validates :name, presence: true
 
   validates :document_number, presence: true
-  validates :document_number, length: { maximum: 14 }
+  validates :document_number, length: { in: 11..14 }
 
   validates :kind, presence: true
-  validates :kind, length: { maximum: 4 }
+  validates :kind, length: { in: 3..4 }
 end
